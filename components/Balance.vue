e706c67d83e21f4f7acf2d7ef440e42b8be19e90
Stephen Wu <jpsiyu@163.com>
2019-07-22 20:57:02 +0800

    add basic framework

---
+++ components/Balance.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="balance">
+    <el-table :data="list" border>
+      <el-table-column prop="report_name" label="日期" />
+      <el-table-column prop="total_assets" label="资产合计" />
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      symbol: '',
+      name: '',
+      list: []
+    }
+  },
+  mounted() {
+    this.$axios.get("/api/balance?symbol=SH601318")
+      .then(res => {
+        const serverMsg = res.data
+        const serverData = serverMsg.data
+        this.symbol = serverData.symbol
+        this.name = serverData.quote_name
+        this.list = serverData.list
+      })
+  },
+}
+</script>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Balance />
+</template>
+
+<script>
+import Balance from '~/components/Balance.vue'
+export default {
+  components: {
+    Balance
+  },
+  methods: {},
+}
+</script>
+
+<style>
+</style>
