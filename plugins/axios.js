c3b3afbfaa409585e4fd450658a3e42871c0d6a6
wusiyu <stephenwu2019@163.com>
2019-10-02 09:55:12 +0800

    product url

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,5 +2,6 @@ import Vue from "vue"
 import axios from 'axios'
 
 Vue.prototype.$axios = axios.create({
-  baseURL: "http://localhost:10000"
+  //baseURL: "http://localhost:10000"
+  baseURL: "http://va.api.blockchainpirate.tech"
 })
