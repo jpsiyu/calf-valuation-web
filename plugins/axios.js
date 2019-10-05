37980429a5739e48ff57abcbc26c6a4dbd925310
wusiyu <stephenwu2019@163.com>
2019-10-05 16:07:10 +0800

    use book

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 
 Vue.prototype.$axios = axios.create({
   //baseURL: "http://localhost:18000"
-  baseURL: "http://api.blockchainpirate.tech"
+  baseURL: "http://book.blockchainpirate.tech"
 })
