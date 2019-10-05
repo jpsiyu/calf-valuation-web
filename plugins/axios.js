cbeea59767b30d6024231d302c80d1b7ce6d963c
wusiyu <stephenwu2019@163.com>
2019-10-05 11:36:16 +0800

    change host

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,6 @@ import Vue from "vue"
 import axios from 'axios'
 
 Vue.prototype.$axios = axios.create({
-  baseURL: "http://localhost:18000"
-  //baseURL: "http://va.blockchainpirate.tech"
+  //baseURL: "http://localhost:18000"
+  baseURL: "http://api.blockchainpirate.tech"
 })
